8c89567f9df559ae8f50d39844e78142ac4ed7b1
vl4deee11 <boi4enkovlad@yandex.ru>
2021-04-11 12:36:39 +0300

    fix

--- main.go
+++ main.go
@@ -42,11 +42,6 @@ func main() {
 		}
 		t := time.Now()
 		discard = append(discard, board.CurrCard)
-		if board.CurrCard.Type == engine.Skip {
-			if err := com.Move(nil); err != nil {
-				panic(err)
-			}
-		}
 
 		opponent := engine.GetOpponentHand(board.Hand, discard)
 		nextCards := engine.GetNextCard(board.Hand, opponent, &board.CurrCard)
