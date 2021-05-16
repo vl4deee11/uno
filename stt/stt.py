7e37629efc0ba33b0e08b8f268a64b9f901c6034
vl4deee11 <boi4enkovlad@yandex.ru>
2021-05-16 10:48:17 +0300

    added stt

---
+++ stt/stt.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+import time
+
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    start_time = time.time()
+    audio = r.listen(source, phrase_time_limit=20)
+
+try:
+    text = r.recognize_google(audio)
+    print(text)
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
